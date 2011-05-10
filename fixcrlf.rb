68d5e77800393d0e937c56e9330a25c50169aa1a
Justin Permar <justin.permar@gmail.com>
2011-05-10 13:04:49 -0400

    Added a Pages controller

--- fixcrlf.rb
+++ fixcrlf.rb
@@ -14,7 +14,7 @@ def list(theDir)
         #puts(full)
         if $files.include?(entry) || $extensions.include?(File.extname(entry))
           #full = File.absolute_path(full)
-          puts "Updating " << full
+          puts "Updating LF -> CRLF for " << full
           str = "dos2unix -D #{full}"
           system(str)
         end
@@ -23,15 +23,6 @@ def list(theDir)
     end
   end
 end
-
-#$files = [ '.gitignore', 'Gemfile.lock', 'README', 'Rakefile', 'application_controller.rb', 'application_helper.rb', 'application.html.erb',
-#  'config.ru', 'application.rb', 'boot.rb', 'database.yml', 'environment.rb', 'development.rb', 'production.rb', 'test.rb', 'backtrace_silencers.rb',
-#  'inflections.rb', 'mime_types.rb', 'secret_token.rb', 'session_store.rb', 'en.yml', 'routes.rb', 'seeds.rb', 'README_FOR_APP', '404.html', '422.html',
-#  '500.html', 'index.html', 'application.js', 'controls.js', 'dragdrop.js', 'effects.js', 'prototype.js', 'rails.js', 'robots.txt', 'rails',
-#  'browsing_test.rb', 'test_helper.rb', 'microposts_controller.rb', 'users_controller.rb', 'microposts_helper.rb', 'users_helper.rb', 'micropost.rb',
-#  'user.rb', '_form.html.erb', 'edit.html.erb', 'index.html.erb', 'new.html.erb', 'show.html.erb', '20110509172106_create_users.rb',
-#  '20110509195359_create_microposts.rb', 'scaffold.css', 'microposts.yml', 'users.yml', 'micropost_controller_test.rb', 'microposts_controller_test.rb',
-#  'users_controller_test.rb', 'microposts_helper_test.rb', 'users_helper_test.rb', 'micropost_test.rb', 'user_test.rb', '.rspec', 'spec_helper.rb' ]
 
 $files = [ '.gitignore', 'Gemfile.lock', 'README', 'Rakefile', 'config.ru', 'README_FOR_APP', 'robots.txt', 'rails', '.rspec' ]
 
